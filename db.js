254f8f1b6f815263824e299f8ef6bdd531dd5b5c
mughees ilyas <mugheesilyas@live.com>
2019-10-18 00:33:35 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 {
-    { id: "1", name: "Canon EOS Rebel SL2", resolution: "24.2" },
+  data:[  { id: "1", name: "Canon EOS Rebel SL2", resolution: "24.2" },
     { id: "2", name: "Fujifilm X100F", resolution: "24.4" },
     { id: "3", name: "Google Pixel 3", resolution: "8" },
     { id: "4", name: "Leica CL", resolution: "24.2" },
@@ -7,5 +7,5 @@
     { id: "6", name: "Olympus PEN-F", resolution: "20.3" },
     { id: "7", name: "Panasonic Lumix DC-G9", resolution: "20.3" },
     { id: "8", name: "Ricoh GR II", resolution: "16.2" },
-    { id: "9", name: "Sony Cyber-Shot DSC-RX1R II", resolution: "42.4" }
+    { id: "9", name: "Sony Cyber-Shot DSC-RX1R II", resolution: "42.4" }]
 };
